2d3fca6c40685d2e05fff7e62fc7539defa0ee96
Rana_Ans <m.ansakhtar2@gmail.com>
2021-12-06 12:07:16 +0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template("user.html", user_name=name)
+
+# Error Pages
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500
