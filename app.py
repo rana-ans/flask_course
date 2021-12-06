c347e198fbceb792c0b2922bde4908f06da8b240
Rana_Ans <m.ansakhtar2@gmail.com>
2021-12-07 00:58:13 +0500

    added navbar logo

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "mysecretkey"
+
+# Create a Form Class
+
 
 # Create a route decorator
 @app.route('/')
@@ -10,7 +17,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template("base.html", user_name=name)
+	return render_template("user.html", user_name=name)
 
 # Error Pages
 @app.errorhandler(404)
