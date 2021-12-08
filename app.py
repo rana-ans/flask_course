761049e99d52c10e73a1a5515a09486d8a34d6eb
Rana_Ans <m.ansakhtar2@gmail.com>
2021-12-08 15:03:18 +0500

    added css-js-images

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -8,12 +8,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"
 
 # Create a Form Class
+class nameForm(FlaskForm):
+	name = StringField("Whats Your Name?", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 
 # Create a route decorator
 @app.route('/')
 def index():
-	return render_template("base.html")
+	flash("Welcome to my Website!")
+	return render_template("index.html")
 
 @app.route('/user/<name>')
 def user(name):
@@ -28,3 +32,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def nameform():
+	name = None
+	form = nameForm()
+	# VALIDATE FORM
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+		flash("Form Submitted Successfully")
+
+	return render_template("nameform.html", 
+		name=name,
+		form=form)
