becdaaa9c33c0d8dea9bf0d9862a1b8bb9bfc5c5
Rana_Ans <m.ansakhtar2@gmail.com>
2021-12-13 12:44:43 +0500

    Added SQLAlchemy db to save User data

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template, flash, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create a Flask Instance
 app = Flask(__name__)
+# Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# Secret Key
 app.config['SECRET_KEY'] = "mysecretkey"
+
+# Initialize the Database
+db = SQLAlchemy(app)
+# Create DB Model
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(120), nullable=False, unique=True)
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+	# Create a String
+	def __repr__(self):
+		return '<Name %r>' % self.name
+
+class userForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 # Create a Form Class
 class nameForm(FlaskForm):
@@ -47,6 +70,31 @@ def nameform():
 	return render_template("nameform.html", 
 		name=name,
 		form=form)
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+	name = None
+	form = userForm()
+	success = None
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			user = Users(name=form.name.data, email=form.email.data)
+			db.session.add(user)
+			db.session.commit()
+			name = form.name.data
+			form.name.data = ''
+			form.email.data = ''
+			flash("User Added Successfully!")
+			success = True
+		else:
+			form.name.data = ''
+			form.email.data = ''
+			flash("Email Already Exists")
+			success = False
+	our_users = Users.query.order_by(Users.date_added)
+	return render_template("add_user.html",
+		form=form, name=name, our_users=our_users, success=success)
 
 @app.route('/insta', methods=['GET', 'POST'])
 def insta():
